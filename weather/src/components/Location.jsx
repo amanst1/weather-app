d7b9aaf8d47c5ae7bfe29d77ba0dfebb95c660ad
amanst1 <seifeamanuel@gmail.com>
2024-02-27 21:37:08 +0300

    add user input state to location jsx, add on change handler function

--- weather/src/components/Location.jsx
+++ weather/src/components/Location.jsx
@@ -1,16 +1,23 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const Location = ({weather_data}) => {
+    const [searchTerm, setSearchTerm] = useState('')
+
     const { name } = weather_data;
     const date = new Date();
     // const month = date.getMonth() + 1;
     // const weekDay = date.getDay();
     // const today = date.getDate();
 
+    const handleInput = (e) => {
+        setSearchTerm(e.target.value)
+    }
+
     return(
         <>
             <h2>Location</h2>
-            <input type='text' />
+            <input type='text' value={searchTerm} onChange={handleInput} />
             <h4>{name}</h4>
             <p> {date.toDateString()}  </p>
         </>
